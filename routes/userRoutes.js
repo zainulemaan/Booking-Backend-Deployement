8cefb39205452722bd3f5faa9a4546634ae48049
zailulemaan <za441568@gmail.com>
2024-10-29 01:26:37 +0500

    Updated Register & Login 7

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require("./../controllers/authController");
 
 router.post("/register", authController.register);
-router.get("/login", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
